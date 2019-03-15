416a96f9e19be75cea466e0fd8d0c6ce6f244fe4
Fan Zhang <vanadiumzhang@gmail.com>
2019-03-15 15:53:08 -0500

    ADD: is balanced symbols

---
+++ basic/my_queue.py
@@ -0,0 +1,15 @@
+class Queue:
+    def __init__(self):
+        self.items = []
+
+    def size(self):
+        return len(self.items)
+
+    def put(self, i):
+        self.items.insert(0, i)
+
+    def get(self):
+        return self.items.pop()
+
+    def empty(self):
+        return self.items == []
---
+++ basic/my_stack.py
@@ -0,0 +1,21 @@
+class Stack(object):
+    def __init__(self):
+        self.items = []
+
+    def __str__(self):
+        return str(self.items)
+
+    def push(self, i):
+        self.items.append(i)
+
+    def peek(self):
+        return self.items[-1]
+
+    def pop(self):
+        return self.items.pop()
+
+    def size(self):
+        return len(self.items)
+
+    def isempty(self):
+        return self.items == []
