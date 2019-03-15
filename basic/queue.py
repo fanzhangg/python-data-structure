a221ca7c35fecd7fe2d612154fb78e49c4ea50ab
Fan Zhang <vanadiumzhang@gmail.com>
2019-03-14 22:00:16 -0500

    ADD: queue ADD: simple balanced parentheses

--- basic/queue.py
+++ basic/queue.py
@@ -2,14 +2,14 @@ class Queue:
     def __init__(self):
         self.items = []
 
+    def size(self):
+        return len(self.items)
+
     def put(self, i):
         self.items.insert(0, i)
 
-    def get(self, i):
-        self.items.pop()
+    def get(self):
+        return self.items.pop()
 
-    def isempty(self):
+    def empty(self):
         return self.items == []
-
-    def size(self):
-        return len(self.items)
